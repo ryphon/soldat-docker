080cc5afd096b39f9883fb5d9e2d658e2b016dbf
Dylan Shepard <dylan@shepard.dev>
2020-08-08 17:14:30 -0700

    final working products

---
+++ oddball/container/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# mv /apps/config.ini /opt/steam/soldat/soldat.ini
+
+# Creating map list based on game mode
+if [ "$GAMEMODE" == "0" ] || [ "$GAMEMODE" == "1" ] || [ "$GAMEMODE" == "2" ] || [ "$GAMEMODE" == "4" ]; then
+  ls /opt/steam/soldat/maps | grep -v ctf_ | grep -v inf_ | grep -v htf_ | cut -d "." -f1 > /opt/steam/soldat/mapslist.txt
+elif [ "$GAMEMODE" == "3" ]; then
+  ls /opt/steam/soldat/maps | grep ctf_ | cut -d "." -f1 > /opt/steam/soldat/mapslist.txt
+elif [ "$GAMEMODE" == "6" ]; then
+  ls /opt/steam/soldat/maps | grep htf_ | cut -d "." -f1 > /opt/steam/soldat/mapslist.txt
+elif [ "$GAMEMODE" == "5" ]; then
+  ls /opt/steam/soldat/maps | grep inf_ | cut -d "." -f1 > /opt/steam/soldat/mapslist.txt
+fi
+
+#chmod +x /opt/steam/soldat/soldatserver
+echo Admin password is: $ADMINPASS
+cd /opt/steam/soldat;
+./soldatserver
---
+++ oddball/soldatoddball.sh
@@ -0,0 +1,5 @@
+docker run -it\
+    -p 23073:23073 \
+    -p 23073:23073/udp \
+    -p 23083:23083 \
+ryphon/soldat-ctf:v1.0
